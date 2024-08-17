78aebe6cc0c7400a675fce00f79e2b8fdb6db027
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-08-17 21:47:55 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from selenium import webdriver
+
+app = Flask(__name__)
+
+@app.route('/run-selenium')
+def run_selenium():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Remote(
+        command_executor='https://standalone-chrome-no5g.onrender.com/',
+        options=options
+    )
+
+    driver.get("https://google.com")
+    title = driver.title
+    driver.quit()
+
+    return jsonify({"title": title})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
